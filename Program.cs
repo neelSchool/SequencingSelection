319ee39ef874dc6470b4921194f6e7ca88ba7e20
Neel Mehendale <21NMehendale@heckgrammar.co.uk>
2024-01-24 10:53:47 +0000

    First Commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,64 @@ namespace SequencingSelection
     {
         static void Main(string[] args)
         {
+            ////Algorithm 1
+            //Console.WriteLine("what is your name?");
+            //string Name = Console.ReadLine();
+            //Console.WriteLine("What is your age?");
+            //int Age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("What is your favorite color?");
+            //string Color = Console.ReadLine();
+
+            ////Algorithm 2
+            //Console.WriteLine("what is your first name?");
+            //string FirstName = Console.ReadLine();
+            //Console.WriteLine(FirstName);
+
+            ////Algorithm 3
+            //Console.WriteLine("What is your surname?");
+            //string Surname = Console.ReadLine();
+            //Console.WriteLine("what is your first name?");
+            //string FirstName = Console.ReadLine();
+            //Console.WriteLine(FirstName);
+            //Console.WriteLine(Surname);
+
+            ////Algorithm 4
+            //Console.WriteLine("what is your first name?");
+            //string FirstName = Console.ReadLine();
+            //Console.WriteLine("What is your surname?");
+            //string Surname = Console.ReadLine();
+            //Console.WriteLine(FirstName + " " + Surname);
+
+            ////Algorithm 5
+            //Console.WriteLine("Enter first number");
+            //int FirstNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter another number");
+            //int SecondNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(FirstNum + SecondNum);
+
+            ////Algorithm 6
+            //Console.WriteLine("Enter first number");
+            //int FirstNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter another number");
+            //int SecondNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(FirstNum + SecondNum);
+            //Console.WriteLine(FirstNum * SecondNum);
+
+            ////Algorithm 7
+            //Console.WriteLine("Enter distance travelled");
+            //int Distance = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter time taken");
+            //int Time = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(Distance / Time);
+
+            ////Algorithm 8
+            //Console.WriteLine("How many minutes have you used this month?");
+            //int Minutes = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("How many texts this month");
+            //int Texts = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine((Minutes * 0.1) + (Texts * 0.05) + 10);
+
+            
 
         }
     }
